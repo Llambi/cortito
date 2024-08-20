ac2068fa1cb76f520527d1901cf3c7f2d151c506
Hugo Perez <hugopf.mail@gmail.com>
2024-08-20 21:05:04 +0200

    feat: Add style to the home

--- assets/js/cortito.js
+++ assets/js/cortito.js
@@ -7,5 +7,11 @@ document.body.addEventListener("htmx:afterRequest", function (evt) {
     const url = `${protocol}//${window.location.hostname}${port}/r/${cortitoUrl.shortUrl}`;
 
     const cortitoUrlDisplay$ = document.querySelector("#cortito-url-display");
+    cortitoUrlDisplay$.classList.add("show");
     cortitoUrlDisplay$.innerHTML = `<a href=${url} target="_blank" rel="noopener noreferrer">${url}</a>`;
+});
+
+document.body.addEventListener("htmx:beforeRequest", function (evt) {
+    const cortitoUrlDisplay$ = document.querySelector("#cortito-url-display");
+    cortitoUrlDisplay$.classList.remove("show");
 });
